cabbaa7f845ddf63fc9407f58c7d8bc346d51aeb
Bharath <bharath@hooq.tv>
2021-03-18 12:35:02 +0800

    Login to EC2

--- mydemos/demo1.sh
+++ mydemos/demo1.sh
@@ -1,2 +1,26 @@
 # Bharath Demo1
 
+bharathdasaraju@MacBook-Pro Downloads $ ssh -i bharathdeveloper.pem ec2-user@13.212.235.115
+Last login: Thu Mar 18 04:31:43 2021 from 111.65.63.61
+
+       __|  __|_  )
+       _|  (     /   Amazon Linux 2 AMI
+      ___|\___|___|
+
+https://aws.amazon.com/amazon-linux-2/
+[ec2-user@ip-172-31-31-18 ~]$
+
+[ec2-user@ip-172-31-31-18 ~]$ cat /etc/*release*
+NAME="Amazon Linux"
+VERSION="2"
+ID="amzn"
+ID_LIKE="centos rhel fedora"
+VERSION_ID="2"
+PRETTY_NAME="Amazon Linux 2"
+ANSI_COLOR="0;33"
+CPE_NAME="cpe:2.3:o:amazon:amazon_linux:2"
+HOME_URL="https://amazonlinux.com/"
+Amazon Linux release 2 (Karoo)
+cpe:2.3:o:amazon:amazon_linux:2
+[ec2-user@ip-172-31-31-18 ~]$
+
